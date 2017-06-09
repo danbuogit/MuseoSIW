7592c29c3de25d1aad7143dc285aeb778bcbedff
Alessandro Attard Barbini <alessandro.attardbarbini@gmail.com>
2017-06-09 12:15:43 +0200

    Redirect to /home if trying to access /login when already logged in

--- museo/src/main/java/com/dbaab/museo/controller/LoginController.java
+++ museo/src/main/java/com/dbaab/museo/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.dbaab.museo.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController
 {
     @GetMapping("/login")
-    public String showLogin()
+    public String showLogin(Principal principal)
     {
-        return "login";
+        boolean alreadyAuthenticated = principal != null;
+        return alreadyAuthenticated ? "redirect:home" : "login";
     }
-    
+
     @RequestMapping("/hello")
     public String hello()
     {
