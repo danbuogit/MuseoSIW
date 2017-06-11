be6979cdc3103f775e8917dae1dd18679721c630
Alessandro Attard Barbini <alessandro.attardbarbini@gmail.com>
2017-06-11 02:27:41 +0200

    Added a reference to the page from where the request first originated

--- museo/src/main/java/com/dbaab/museo/controller/PaintingController.java
+++ museo/src/main/java/com/dbaab/museo/controller/PaintingController.java
@@ -1,5 +1,7 @@
 package com.dbaab.museo.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -11,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.dbaab.museo.model.Artist;
@@ -28,7 +31,9 @@ public class PaintingController
     private ArtistService artistService;
     
     @GetMapping("/painting/modify")
-    public String showModifyForm(Model model, @RequestParam(value = "id", required = true) Long id)
+    public String showModifyForm(Model model,
+            @RequestParam(value = "id", required = true) Long id,
+            @RequestHeader(value = "referer", required = false) final String referer)
     {
         Painting painting = this.paintingService.findById(id);
         if (painting == null)
@@ -36,23 +41,43 @@ public class PaintingController
         
         List<Artist> artists = artistService.findAllOrderedByName();
         
+        model.addAttribute("ref", referer != null ? referer : "redirect:galleryController");
         model.addAttribute("painting", painting);
         model.addAttribute("artists", artists);
         return "painting-modify";
     }
 
     @PostMapping("/painting/modify")
-    public String modify(@Valid @ModelAttribute("painting") Painting painting, BindingResult bindingResult, Model model)
+    public String modify(Model model,
+            @RequestParam(value = "ref", required = true) String referer,
+            @Valid @ModelAttribute("painting") Painting painting,
+            BindingResult bindingResult)
     {
+        
         if (bindingResult.hasErrors())
         {
             List<Artist> artists = artistService.findAllOrderedByName();
+            model.addAttribute("ref", referer);
             model.addAttribute("artists", artists);
             return "painting-modify";
         }
         
         System.out.println(painting.toString());
         paintingService.save(painting);
-        return "painting-modify";
+        
+        String next = "redirect:galleryController";
+        try
+        {
+            URI uri = new URI(referer);
+            next = uri.getPath();
+        }
+        catch (URISyntaxException e) { }
+        
+        if (next.startsWith("/"))
+            next = next.substring(1);
+        if (next.isEmpty())
+            next = "home";
+        next = "redirect:" + next;
+        return next;
     }
 }
