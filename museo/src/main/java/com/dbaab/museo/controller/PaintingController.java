a15b70b17a85cd72e40f56d320af5fbe208daf61
Daniele <daniele.buonadonna95@gmail.com>
2017-06-12 12:54:25 +0200

    Adesso è possibile aggiungere ed eliminare un quadro in galleria.
    Nel caso in cui si stia eliminando un quadro, viene richiesta la
    conferma esplicita, per motivi di sicurezza.

--- museo/src/main/java/com/dbaab/museo/controller/PaintingController.java
+++ museo/src/main/java/com/dbaab/museo/controller/PaintingController.java
@@ -9,9 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.dbaab.museo.model.Artist;
 import com.dbaab.museo.model.Painting;
@@ -19,15 +22,15 @@ import com.dbaab.museo.service.ArtistService;
 import com.dbaab.museo.service.PaintingService;
 
 @Controller
-@RequestMapping("/addPaintingController")
-public class AddPaintingController {
+public class PaintingController {
 	
 	@Autowired
 	PaintingService serviceP;
+	
 	@Autowired
 	ArtistService serviceA;
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping("/admins/painting/add")
 	public String retrieveArtistsInfo(Model model)
 	{
 		List<Artist> artistList = serviceA.findAllOrderedByName();
@@ -36,23 +39,25 @@ public class AddPaintingController {
 		return "addPaintingForm";
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping("/admins/painting/add")
 	public String addPainting(@Valid @ModelAttribute Painting painting, BindingResult bindingResult, Model model)
 	{
 		if(bindingResult.hasErrors())
 			return "addPaintingForm";
-		else if (!controllYear(painting.getYear())){
+		else if (!controlYear(painting.getYear())){
 			model.addAttribute("errorYear", "Year not valid.");
 			return retrieveArtistsInfo(model);
 		}
 		else{
-			model.addAttribute("newPainting", painting);
+			model.addAttribute("painting", painting);
+			model.addAttribute("isRemoving", false);
 			serviceP.save(painting);
 		}
 		return "summary";
 	}
 
-	private boolean controllYear(Integer year) {
+	private boolean controlYear(Integer year)
+	{
 		
 		LocalDate now = LocalDate.now();
 		int yearNow = now.getYear();
@@ -60,5 +65,32 @@ public class AddPaintingController {
 		if(yearNow >= year)
 			return true;
 		return false;
+	}
+	
+	@GetMapping("/admins/painting/remove")
+	public String showInfoPainting(Model model,
+			@RequestParam(value="id", required=true) Long id)
+	{
+		Painting painting = serviceP.findById(id);
+		model.addAttribute("painting", painting);
+		model.addAttribute("isRemoving", true);
+		return "summary";
+	}
+	
+	@PostMapping("/admins/painting/remove")
+	public String removePainting(Model model,
+			@RequestParam(value="dec", required=true) String decision,
+			@RequestParam(value="id", required=true) Long id)
+	{
+		if(decision.equals("y"))
+		{
+			this.serviceP.delete(id);
+			model.addAttribute("removed", true);
+		}
+		else
+			model.addAttribute("removed", false);
+		
+		return "redirectionPage";
+		
 	}
 }
