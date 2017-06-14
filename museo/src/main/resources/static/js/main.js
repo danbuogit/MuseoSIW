9358ca92c254b292707ac1dbd0f2c85fb1658135
Alessandro Attard Barbini <alessandro.attardbarbini@gmail.com>
2017-06-14 22:23:07 +0200

    Applied the 'footer' fragment to the templates

--- museo/src/main/resources/static/js/main.js
+++ museo/src/main/resources/static/js/main.js
@@ -1,18 +1,17 @@
 function switchLanguage(lang)
 {
     var currentUrl = window.location.href;
+    
+    // If the current url contains a fragment identifier (#), then remove it.
+    var url = currentUrl.replace(/#.*([?&#]|$)/ig, '$1');
 
     // If the query string already contains 'lang', then change its value.
     if (currentUrl.indexOf('lang=') > -1)
-        var url = currentUrl
-                .replace(/(lang=).*([?&#]|$)/ig, '$1' + lang + '$2');
+        url = currentUrl.replace(/(lang=).*([?&#]|$)/ig, '$1' + lang + '$2');
     else
     {
-        if (currentUrl.endsWith('#'))
-            currentUrl = currentUrl.substring(0, currentUrl.length - 1);
-
         var q = (currentUrl.indexOf('?') > -1) ? '&' : '?';
-        var url = currentUrl + q + 'lang=' + lang;
+        url = currentUrl + q + 'lang=' + lang;
     }
 
     window.location.href = url;
